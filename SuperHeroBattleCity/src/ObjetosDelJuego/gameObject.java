d3f30bda720d7278cb33063262eb660c801dadcd
tomasfelder <tomas.felder@gmail.com>
2016-09-07 23:15:28 -0300

    Primer Commit

---
+++ SuperHeroBattleCity/src/ObjetosDelJuego/gameObject.java
@@ -0,0 +1,20 @@
+package ObjetosDelJuego;
+import javax.swing.*;
+
+public abstract class gameObject {
+
+	protected int x;
+	protected int y;
+	protected ImageIcon imagen;
+	
+	abstract boolean colision(gameObject g);
+	
+	public int x(){
+		return x;
+	}
+	
+	public int y(){
+		return y;
+	}
+	
+}
