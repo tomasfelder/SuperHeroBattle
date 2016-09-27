e0df291740b38163dc3d5303e424c5d7b1b74987
tomasfelder <tomas.felder@gmail.com>
2016-09-27 13:38:38 -0300

    Patron de diseño State funcionando!!

--- SuperHeroBattleCity/src/ObjetosDelJuego/Jugador.java
+++ SuperHeroBattleCity/src/ObjetosDelJuego/Jugador.java
@@ -9,19 +9,8 @@ public class Jugador extends Tanque{
 	protected Nivel miNivel;
 	
 	public Jugador(int x,int y){
-		
 		super(x,y);
 		miNivel=new Nivel_1();
-		velocidadMovimiento=2;
-		golpesQueResiste=1;
-		velocidadDisparo=1;
-		
-//		imagen=new ImageIcon(this.getClass().getResource("/imagenesJugador/Nivel_1_Der.png"));
-//		
-//		iconos[0]=new ImageIcon(this.getClass().getResource("/imagenesJugador/Nivel_1_Arriba.png"));
-//		iconos[1]=new ImageIcon(this .getClass().getResource("/imagenesJugador/Nivel_1_Abajo.png"));
-//		iconos[2]=new ImageIcon(this.getClass().getResource("/imagenesJugador/Nivel_1_Izq.png"));
-//		iconos[3]=new ImageIcon(this.getClass().getResource("/imagenesJugador/Nivel_1_Der.png"));
 	}
 	
 	public void setNivel(int n){
